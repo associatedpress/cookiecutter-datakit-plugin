8a6baab97ae1208a73391b90fdf7d6b40666c682
Serdar Tumgoren <zstumgoren@gmail.com>
2017-01-27 14:54:51 -0800

    Fix example test

--- {{cookiecutter.repo_root}}/tests/test_greet.py
+++ {{cookiecutter.repo_root}}/tests/test_greet.py
@@ -9,7 +9,7 @@ from {{ cookiecutter.project_slug }}.greet import Greet
 def test_greet(capsys):
     """Sample pytest test function with a built-in pytest fixture as an argument.
     """
-    # cmd = Greeting(None, None, cmd_name='{{cookiecutter.project_slug':greet')
+    # cmd = Greeting(None, None, cmd_name='fancyplugin:greet')
     # parsed_args = mock.Mock()
     # parsed_args.greeting = 'Hello world!'
     # cmd.run(parsed_args)
